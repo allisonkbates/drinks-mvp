4505e55686e5f60cecaa1c2c494eed047c7f4251
Allison Bates <allisonkbates@gmail.com>
2020-07-04 19:28:47 -0400

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,44 @@
-import Link from 'next/link'
-import fetch from 'node-fetch'
+import React, { Component } from 'react';
+import fetch from 'node-fetch';
 
-function Index({ stars }) {
+class Index extends Component {
+  render() {
+    const drinks = this.props.drinks;
+    const showDrinks = drinks.map((drink) => {
+      return <DrinkCard key={drink.id} drink={drink}/>
+    });
+    return (
+      <div>
+        {showDrinks}
+        <p>Hello</p>
+      </div>
+    )
+  }
+}
+
+class DrinkCard extends Component {
+  render() {
+    const drink = this.props.drink;
   return (
     <div>
-      <p>Next.js has {stars} ⭐️</p>
-      <Link href="/preact-stars">
-        <a>How about preact?</a>
-      </Link>
+      <h1>{drink.fields["Cocktail Name"]}</h1>
+      <p>{drink.fields["Specified Ingredients"]}</p>
+      <p>{drink.fields["Preparation"]}</p>
     </div>
   )
+  }
 }
 
 export async function getStaticProps() {
-  const res = await fetch('https://api.github.com/repos/vercel/next.js')
+  const apiKey = process.env.API_KEY;
+  const res = await fetch(`https://api.airtable.com/v0/app53cV9D5L6e38bR/Drinks?api_key=${apiKey}`);
   const json = await res.json()
 
   return {
     props: {
-      stars: json.stargazers_count,
+      drinks: json.records,
     },
   }
 }
 
-export default Index
+export default Index;
